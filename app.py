256500c7b52c143dfe169dc8997d074bac133d66
Sergio <sergio.soto1@mail.udp.cl>
2024-06-10 20:46:41 -0400

    Actualizar los requisitos

--- app.py
+++ app.py
@@ -1,29 +1,10 @@
-from flask import Flask, request, jsonify
-import psycopg2
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# Configuración de la base de datos
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-def check_db_connection():
-    try:
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
-        conn.close()
-        print("Conexión a la base de datos exitosa!")
-        return True
-    except psycopg2.Error as e:
-        print("Error connecting to PostgreSQL:", e)
-        return False
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
