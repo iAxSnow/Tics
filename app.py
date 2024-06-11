b1fa748a4736ca8aa7a3c3ccbd8d720d8030106c
SergioSoto1 <166153882+SergioSoto1@users.noreply.github.com>
2024-06-10 21:27:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import psycopg2
 import os
 
 app = Flask(__name__)
 
 # Configuración de la base de datos
-DB_HOST = "tics.cpuae6qmuvit.us-east-2.rds.amazonaws.com"
-DB_NAME = "tics"
-DB_USER = "postgres"
-DB_PASSWORD = "postgresql"
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 def check_db_connection():
     try:
         conn = psycopg2.connect(
             host=DB_HOST,
+            port=DB_PORT,
             database=DB_NAME,
             user=DB_USER,
             password=DB_PASSWORD
@@ -25,7 +27,12 @@ def check_db_connection():
         print("Error connecting to PostgreSQL:", e)
         return False
 
-
+@app.route('/')
+def index():
+    if check_db_connection():
+        return jsonify({"message": "Conexión exitosa a la base de datos!"})
+    else:
+        return jsonify({"message": "Error al conectar a la base de datos"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=True)
