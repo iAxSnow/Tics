b4e305301e1e6386a7e1f02e9fc36c5d121b6a75
Sergio <sergio.soto1@mail.udp.cl>
2024-06-10 21:28:34 -0400

    Inicializar proyecto en Heroku

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 
 # Configuración de la base de datos
 DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 def check_db_connection():
     try:
         conn = psycopg2.connect(
             host=DB_HOST,
+            port=DB_PORT,
             database=DB_NAME,
             user=DB_USER,
             password=DB_PASSWORD
@@ -27,7 +29,10 @@ def check_db_connection():
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Hello, World!"})
+    if check_db_connection():
+        return jsonify({"message": "Conexión exitosa a la base de datos!"})
+    else:
+        return jsonify({"message": "Error al conectar a la base de datos"})
 
 if __name__ == '__main__':
     app.run(debug=True)
