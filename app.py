226288f65977cf900dc3df051c76bece420e7fdc
Sergio <sergio.soto1@mail.udp.cl>
2024-06-10 22:53:27 -0400

    Actualizar archivo app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-def check_db_connection():
+def get_db_connection():
     try:
         conn = psycopg2.connect(
             host=DB_HOST,
@@ -23,12 +23,49 @@ def check_db_connection():
             user=DB_USER,
             password=DB_PASSWORD
         )
-        conn.close()
         print("Conexión a la base de datos exitosa!")
-        return True
+        return conn
     except psycopg2.Error as e:
         print("Error connecting to PostgreSQL:", e)
-        return False
+        return None
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    conn = get_db_connection()
+    if conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM lecturas_sensor')
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify(rows)
+    else:
+        return jsonify({"error": "No se pudo conectar a la base de datos"})
+
+@app.route('/data', methods=['POST'])
+def post_data():
+    data = request.json
+    if not data:
+        return jsonify({"error": "No se proporcionaron datos"})
+    
+    conn = get_db_connection()
+    if conn:
+        cur = conn.cursor()
+        for item in data:
+            fecha_hora = item.get('fecha_hora')
+            ph = item.get('ph')
+            humedad = item.get('humedad')
+            temperatura = item.get('temperatura')
+            usuario_rut = item.get('usuario_rut')
+
+            cur.execute('INSERT INTO lecturas_sensor (fecha_hora, ph, humedad, temperatura, usuario_rut) VALUES (%s, %s, %s, %s, %s)',
+                        (fecha_hora, ph, humedad, temperatura, usuario_rut))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return jsonify({"message": "Datos insertados correctamente"})
+    else:
+        return jsonify({"error": "No se pudo conectar a la base de datos"})
 
 @app.route('/')
 def index():
